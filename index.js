f23ee2dfe8d192958fe2d7a76427fa81886508e7
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2025-07-10 16:30:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 
 app.use(express.json());
