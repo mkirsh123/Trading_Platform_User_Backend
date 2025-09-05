7183b4892008889570929e53644301bae5b1fe7c
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2025-09-05 23:23:52 +0530

    Update UserModel.js

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 8);
 });
 
 module.exports = mongoose.model("User", userSchema);
+
