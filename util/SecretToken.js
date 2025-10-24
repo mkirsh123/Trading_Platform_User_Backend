c9cada735bef191f80827be78627aee82317c379
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2025-10-24 20:12:13 +0530

    Update SecretToken.js

--- util/SecretToken.js
+++ util/SecretToken.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id) => {
-  return jwt.sign({ id }, process.env.TOKEN_KEY, {
+  const privateKey = process.env.PRIVATE_KEY;
+  return jwt.sign({ id }, privateKey, {
     expiresIn: 3 * 24 * 60 * 60,
+    algorithm: "RS256",
   });
 };
